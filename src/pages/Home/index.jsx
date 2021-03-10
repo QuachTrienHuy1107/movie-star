a1df014c1997621a58d363566472476955738d5c
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-03-11 01:00:21 +0700

    move code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,25 +7,15 @@ import "./style.scss";
 import MovieList from "../../components/MovieList";
 import Carousel from "../../components/Carousel";
 import axios from "axios";
+import productApi from "../../services/movie";
 
 function Home() {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        async function getAllMovies() {
-            const request = await axios.get("http://svcy.myclass.vn/api/Movie/GetMovie");
-            setMovies(request.data);
-        }
-        return getAllMovies();
-    }, []);
-
     return (
         <div>
-            {console.log("movies", movies)}
             <Carousel />
             <Container fluid style={{ width: "85%" }}>
-                <MovieList title={"Reccommend"} />
-                <MovieList title={"Top Rating"} logo={"1"} items={10} allMovie={movies} />
+                <MovieList title={"Reccommend"} items={6} />
+                <MovieList title={"Top Rating"} logo={"1"} />
             </Container>
         </div>
     );
